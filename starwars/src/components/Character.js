9c98f668fee4abbb6180a02ad3ca26356e978f89
cwern40 <chris.wernli@hotmail.com>
2019-06-08 11:51:52 -0600

    I added code to render the name on the screen from each character

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './StarWars.css'
 
 const Character = props => {
     console.log("Character Component", props);
     return (
         <div className="character">
-
+            <h2 className="character-name">
+            {props.character.name}
+            </h2>
         </div>
     );
 };
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -5,7 +5,9 @@ const CharacterList = props => {
     console.log("Character List:", props);
     return (
         <div className="character-list">
-
+            {props.characters.map(character => (
+                <Character character={character} key={character.created}/>
+            ))}
         </div>
     );
 };

fb9695edea08c6112ae8a0461b116c4db3cf68e4
cwern40 <chris.wernli@hotmail.com>
2019-06-08 10:40:25 -0600

    I created the CharacterList component

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import Character from './Character';
+
+const CharacterList = props => {
+    console.log("Character List:", props);
+    return (
+        <div className="character-list">
+            <Character />
+        </div>
+    );
+};
+
+export default CharacterList;

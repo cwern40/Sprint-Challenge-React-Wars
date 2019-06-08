a6c0b42a3624c4231e7ce3722974572b7643c96a
cwern40 <chris.wernli@hotmail.com>
2019-06-08 10:38:19 -0600

    I created the Character.js file and created the Character component.

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Character = props => {
+    console.log("Character Component", props);
+    return (
+        <div className="character">
+
+        </div>
+    );
+};
+
+export default Character;
